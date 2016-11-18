10d4bea3bed0690ddf042356550f2146b8e6868b
frank.he <frank.he@logicpd.com>
2016-11-18 02:12:31 -0600

    Reposition the gpm and xpm widgets.

--- dashdota/Dashboard/Scripts/Dashdota/Widgets/NetworthWidget.js
+++ dashdota/Dashboard/Scripts/Dashdota/Widgets/NetworthWidget.js
@@ -9,14 +9,14 @@
     background.src = BaseImageDirectoryUrl + "networth_background.png"
     background.style.position = "absolute";
     background.style.zIndex = 0;
-    background.style.left = "120px";
+    background.style.left = "0px";
     background.style.top = "0px";
     container.appendChild(background);
 
     var heroPortrait = document.createElement("img");
     heroPortrait.style.position = "absolute";
     heroPortrait.style.zIndex = 1;
-    heroPortrait.style.left = "121px";
+    heroPortrait.style.left = "1px";
     heroPortrait.style.top = "2px";
     container.appendChild(heroPortrait);
 
@@ -24,66 +24,48 @@
     networthNumberBox.className = "networthNumberBox";
     networthNumberBox.style.position = "absolute";
     networthNumberBox.style.zIndex = 1;
-    networthNumberBox.style.left = "262px";
+    networthNumberBox.style.left = "140px";
     networthNumberBox.style.top = "12px";
     container.appendChild(networthNumberBox);
 
     var nameTextBox = document.createElement("div");
     nameTextBox.className = "nameTextBox";
     nameTextBox.style.zIndex = 1;
-    nameTextBox.style.left = "124px";
+    nameTextBox.style.left = "4px";
     nameTextBox.style.top = "54px";
     container.appendChild(nameTextBox);
-
-    var gpmBackgroundBox = document.createElement("img");
-    gpmBackgroundBox.src = BaseImageDirectoryUrl + "value_box_olive.png";
-    gpmBackgroundBox.style.position = "absolute";
-    gpmBackgroundBox.style.zIndex = 0;
-    gpmBackgroundBox.style.left = "0px";
-    gpmBackgroundBox.style.top = "2px";
-    container.appendChild(gpmBackgroundBox);
-
-    var xpmBackgroundBox = document.createElement("img");
-    xpmBackgroundBox.src = BaseImageDirectoryUrl + "value_box_teal.png";
-    xpmBackgroundBox.style.position = "absolute";
-    xpmBackgroundBox.style.zIndex = 0;
-    xpmBackgroundBox.style.left = "0px";
-    xpmBackgroundBox.style.top = "38px";
-    container.appendChild(xpmBackgroundBox);
 
     var gpmLabelBox = document.createElement("div");
     gpmLabelBox.className = "smallLabelBox";
     gpmLabelBox.innerHTML = "gpm:";
     gpmLabelBox.style.zIndex = 1;
-    gpmLabelBox.style.left = "5px";
-    gpmLabelBox.style.top = "9px";
+    gpmLabelBox.style.left = "132px";
+    gpmLabelBox.style.top = "56px";
     container.appendChild(gpmLabelBox);
 
     var xpmLabelBox = document.createElement("div");
     xpmLabelBox.className = "smallLabelBox";
     xpmLabelBox.innerHTML = "xpm:";
     xpmLabelBox.style.zIndex = 1;
-    xpmLabelBox.style.left = "5px";
-    xpmLabelBox.style.top = "45px";
+    xpmLabelBox.style.left = "200px";
+    xpmLabelBox.style.top = "56px";
     container.appendChild(xpmLabelBox);
 
     var gpmNumberBox = document.createElement("div");
     gpmNumberBox.className = "smallNumberBox";
     gpmNumberBox.style.zIndex = 1;
-    gpmNumberBox.style.left = "50px";
-    gpmNumberBox.style.top = "3px";
-    gpmNumberBox.style.color = "goldenrod";
+    gpmNumberBox.style.left = "162px";
+    gpmNumberBox.style.top = "57px";
     container.appendChild(gpmNumberBox);
 
     var xpmNumberBox = document.createElement("div");
     xpmNumberBox.className = "smallNumberBox";
     xpmNumberBox.style.zIndex = 1;
-    xpmNumberBox.style.left = "50px";
-    xpmNumberBox.style.top = "39px";
-    xpmNumberBox.style.color = "purple";
+    xpmNumberBox.style.left = "230px";
+    xpmNumberBox.style.top = "57px";
     container.appendChild(xpmNumberBox);
 
-    var networthBarWidget = new NetworthBarWidget(container, "247px", "26px", 0);
+    var networthBarWidget = new NetworthBarWidget(container, "127px", "26px", 0);
 
     // External Functions
     container.SetName = function (value) {
