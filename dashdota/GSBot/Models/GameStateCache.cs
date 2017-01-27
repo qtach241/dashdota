5c6a912cfae4716930e1872bb5883ccc3fffebf5
frank.he <frank.he@logicpd.com>
2017-01-27 12:23:58 -0600

    GSBot app is now in a working state.
    - Added Logging.
    - Added GameStateCache
    - Added pull timers and sounds
    - Added app versioning.
    - Added script compile as build event.

---
+++ dashdota/GSBot/Models/GameStateCache.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSBot.Models
+{
+    public class GameStateCache
+    {
+        public GameStateCache()
+        {
+        }
+
+        public void UpdateCache(Dota2GSI.GameState gs)
+        {
+            SessionPackets++;
+
+            ClockTime = gs?.Map?.ClockTime;
+            HeroName = gs?.Hero?.Name;
+
+            if (ClockTime != null)
+            {
+                GameClock = TimeSpan.FromSeconds(gs.Map.ClockTime);
+            }
+
+            PullReadyFlag = ((GameClock.Minutes < 30) && IsEven(GameClock.Minutes) && (GameClock.Seconds > 38));
+            PullCountdownFlag = ((GameClock.Minutes < 30) && IsEven(GameClock.Minutes) && (GameClock.Seconds > 43));
+        }
+
+        private static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+
+        public int SessionPackets { get; set; } = 0;
+        public string HeroName { get; set; }
+        public int? ClockTime { get; set; }
+        public TimeSpan GameClock { get; set; }
+        public bool PullReadyFlag { get; set; } = false;
+        public bool PullCountdownFlag { get; set; } = false;
+    }
+}
