41054c353264a72bbea445065c85a6d223272344
frank.he <frank.he@logicpd.com>
2017-09-29 10:54:05 -0500

    Rename SupportHelper to AudioAlerts. Stub midas ready alert.

--- dashdota/GSBot/Helpers/AudioAlerts.cs
+++ dashdota/GSBot/Helpers/AudioAlerts.cs
@@ -3,19 +3,21 @@ using System.IO;
 
 namespace GSBot.Helpers
 {
-    public class SupportHelper
+    public class AudioAlerts
     {
-        static string soundsDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Sounds/";
+        static string soundsDir = Directory
+            .GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "/Sounds/";
 
         /// <summary>
         /// Plays a sound to alert player to stack/pull camps soon.
         /// </summary>
         /// <param name="state"></param>
-        public static void PullTimers(MasterState state)
+        public static void StackCamps(MasterState state)
         {
             if (!state.sPullReadyFlag && state.PullReadyFlag)
             {
-                using (System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundsDir + "jensen_ready.wav"))
+                using (System.Media.SoundPlayer player = new System.Media
+                    .SoundPlayer(soundsDir + "jensen_ready.wav"))
                 {
                     player.Play();
                 }
@@ -23,7 +25,8 @@ namespace GSBot.Helpers
 
             if (!state.sPullCountdownFlag && state.PullCountdownFlag)
             {
-                using (System.Media.SoundPlayer player = new System.Media.SoundPlayer(soundsDir + "jensen_countdown.wav"))
+                using (System.Media.SoundPlayer player = new System.Media
+                    .SoundPlayer(soundsDir + "jensen_countdown.wav"))
                 {
                     player.Play();
                 }
@@ -34,10 +37,19 @@ namespace GSBot.Helpers
         }
 
         /// <summary>
+        /// Plays a sound to alert player when midas is off cooldown.
+        /// </summary>
+        /// <param name="state"></param>
+        public static void MidasReady(MasterState state)
+        {
+
+        }
+
+        /// <summary>
         /// Plays a sound to alert player that tree heal is off cooldown. Heal a tower!
         /// </summary>
         /// <param name="state"></param>
-        public static void TreantTimer(MasterState state)
+        public static void LivingArmorReady(MasterState state)
         {
             if (state.Abilities[2].Cooldown == 0)
             {
