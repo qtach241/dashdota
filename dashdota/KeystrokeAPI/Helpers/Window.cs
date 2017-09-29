d2730b79555a9ae1505a140a52d4a9821500a41e
frank.he <frank.he@logicpd.com>
2017-09-29 17:28:31 -0500

    Import Keystroke API project files. Builds successfully.

---
+++ dashdota/KeystrokeAPI/Helpers/Window.cs
@@ -0,0 +1,20 @@
+﻿using KeystrokeAPI;
+using System.Text;
+
+namespace KeystrokeAPI.Helpers
+{
+    internal class Window
+    {
+        internal string CurrentWindowTitle()
+        {
+            int hwnd = User32.GetForegroundWindow();
+            StringBuilder title = new StringBuilder(1024);
+
+            int textLength = User32.GetWindowText(hwnd, title, title.Capacity);
+            if ((textLength <= 0) || (textLength > title.Length))
+                return "[Unknown]";
+
+            return "[" + title + "]";
+        }
+    }
+}
