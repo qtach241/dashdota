4692cd834555e054aaa9bcd64a0ea876d0a99603
frank.he <frank.he@logicpd.com>
2017-09-30 01:42:01 -0500

    Add Console Tester project for local testing.

---
+++ dashdota/ConsoleTester/Program.cs
@@ -0,0 +1,30 @@
+﻿using KeystrokeClient;
+using System;
+using System.Windows.Forms;
+using GSBot.Helpers;
+
+namespace ConsoleApplicationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var api = new KeystrokeListener())
+            {
+                api.CreateKeyboardHook((character) =>
+                {
+                    switch (character.KeyCode)
+                    {
+                        case KeyCode.M:
+                            Invoker.InvokeDeafeningBlast();
+                            break;
+                    }
+                    Console.Write(character);
+                });
+                Application.Run();
+                //Console.ReadKey();
+            }
+            //Console.ReadKey();
+        }
+    }
+}
